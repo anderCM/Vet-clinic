6d302c4f27e835eff1771f95248e173e98798df3
anderCM <andercm15@gmail.com>
2023-04-06 18:05:32 -0500

    vets and visits tables created

--- schema.sql
+++ schema.sql
@@ -28,3 +28,18 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals
 	ADD COLUMN species_id INTEGER REFERENCES species(id),
 	ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+CREATE TABLE specializations(
+	species_id INTEGER NOT NULL,
+	vet_id INTEGER NOT NULL,
+	FOREIGN KEY (species_id) REFERENCES species(id),
+	FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits(
+	animal_id INTEGER NOT NULL,
+	vet_id INTEGER NOT NULL,
+	date_of_visit DATE NOT NULL,
+	FOREIGN KEY (animal_id) REFERENCES animals(id),
+	FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
