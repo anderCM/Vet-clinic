23cf52f771cb50dacf14239ce292108c270d0402
elad237 <tumbu.elad@gmail.com>
2023-04-12 15:33:59 +0100

    Updated the schema_based_on_diagram.sql file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,12 @@
-CREATE TABLE invoice_items {
+-- Create a table named invoice_items  with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+--unit_price        DECIMAL,
+ -- quantity          INT,
+  --total_price       DECIMAL,
+  --invoice_id        INT REFERENCES invoices(id),
+  --treatment_id      INT REFERENCES treatments(id),
+  --PRIMARY KEY(id, invoice_id, treatment_id)
+CREATE TABLE invoice_items (
   id                INT GENERATED ALWAYS AS IDENTITY,
   unit_price        DECIMAL,
   quantity          INT,
@@ -6,4 +14,4 @@ CREATE TABLE invoice_items {
   invoice_id        INT REFERENCES invoices(id),
   treatment_id      INT REFERENCES treatments(id),
   PRIMARY KEY(id, invoice_id, treatment_id)
-}
+);
