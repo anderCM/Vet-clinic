79df85b64382209c9f41b5ea1ca12485afc8cf86
anderCM <andercm15@gmail.com>
2023-04-12 09:38:51 -0500

    patients and invoices tables added

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE vet_clinic_diagram;
 -- Create a table named invoice_items  with the following columns:
 -- id: integer (set it as autoincremented PRIMARY KEY)
 --unit_price        DECIMAL,
@@ -14,4 +15,18 @@ CREATE TABLE invoice_items (
   invoice_id        INT REFERENCES invoices(id),
   treatment_id      INT REFERENCES treatments(id),
   PRIMARY KEY(id, invoice_id, treatment_id)
+);
+
+CREATE TABLE patients(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	date_of_birth DATE
+);
+
+CREATE TABLE invoices(
+	id SERIAL PERIMARY KEY,
+	total_amount DECIMAL NOT NULL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INTEGER REFERENCES medical_histories(id)
 );
